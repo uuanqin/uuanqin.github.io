8592cdec0c25f1761c3ecac3f5fa3a212daf7863
wuanqin <uuanqin@qq.com>
2023-07-24 22:45:46 +0800

    Site updated: 2023-07-24 22:45:44

--- js/float.js
+++ js/float.js
@@ -20,7 +20,7 @@ var allowAll = false; // true 则允许所有网页存在漂浮物
 var urlAllowList = [
     "/about/",
     "/friends/",
-    "/p/28a4bfe2.html", // 这是介绍 float的那篇文章
+    "/p/28a4bfe2/", // 这是介绍 float的那篇文章
 ]
 
 var isAllowFloat = false;   // 全局变量，允许使用漂浮特效
