9208af934c139c52101aeabb603620c3a615f7f5
wuanqin <wuanqin@mail.ustc.edu.cn>
2024-11-27 23:46:34 +0800

    Site updated: 2024-11-27 23:46:30

--- js/random.js
+++ js/random.js
@@ -1,11 +1,16 @@
+// 由张洪 Heo 的代码改动而来
 function randomPost() {
-    fetch('/baidusitemap.xml').then(res => res.text()).then(str => (new window.DOMParser()).parseFromString(str, "text/xml")).then(data => {
-        let ls = data.querySelectorAll('url loc');
-        while (true) {
-            let url = ls[Math.floor(Math.random() * ls.length)].innerHTML;
-            if (location.href == url) continue;
-            location.href = url;
-            return;
-        }
-    })
+    fetch('/baidusitemap.xml')
+        .then(res => res.text())
+        .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+        .then(data => {
+            let ls = data.querySelectorAll('url loc');
+            while (true) {
+                let url = ls[Math.floor(Math.random() * ls.length)].innerHTML;
+                let path = new URL(url).pathname; // 提取路径部分
+                if (location.pathname === path) continue;
+                location.pathname = path;
+                return;
+            }
+        })
 }
