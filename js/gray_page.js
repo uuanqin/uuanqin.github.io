fd4d85869d5139b7c8f3f0bac9d28d7efefbc071
wuanqin <wuanqin@mail.ustc.edu.cn>
2025-06-17 00:05:44 +0800

    Site updated: 2025-06-17 00:05:40

--- js/gray_page.js
+++ js/gray_page.js
@@ -1,7 +1,4 @@
-let style = document.createElement('style')
-let graySelector = 'gray-filter'
-style.setAttribute('type', 'text/css')
-style.textContent = `.${graySelector}{
+let style=document.createElement('style'),graySelector='gray-filter',root=(style.setAttribute('type','text/css'),style.textContent=`.${graySelector}{
   -webkit-filter: grayscale(100%);
   -moz-filter: grayscale(100%);
   -ms-filter: grayscale(100%);
@@ -9,14 +6,4 @@ style.textContent = `.${graySelector}{
   filter: grayscale(100%);
   filter: gray;
   filter: progid:dximagetransform.microsoft.basicimage(grayscale=1);
-}`
-document.head.appendChild(style)
-
-let root = document.querySelector('html')
-url = window.location.pathname
-toggleClassName(root,graySelector)
-
-function toggleClassName(el,name){
-    if(url==="/p/26a31d07/") // 指定你要变灰的网页
-        el.className = [el.className, name].join(' ')
-}
+}`,document.head.appendChild(style),document.querySelector('html'));function toggleClassName(el,name){'/p/26a31d07/'===url&&(el.className=[el.className,name].join(' '))}url=window.location.pathname,toggleClassName(root,graySelector);
--- js/random.js
+++ js/random.js
@@ -1,16 +1,1 @@
-// 由张洪 Heo 的代码改动而来
-function randomPost() {
-    fetch('/baidusitemap.xml')
-        .then(res => res.text())
-        .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
-        .then(data => {
-            let ls = data.querySelectorAll('url loc');
-            while (true) {
-                let url = ls[Math.floor(Math.random() * ls.length)].innerHTML;
-                let path = new URL(url).pathname; // 提取路径部分
-                if (location.pathname === path) continue;
-                location.pathname = path;
-                return;
-            }
-        })
-}
+function randomPost(){fetch('/baidusitemap.xml').then(res=>res.text()).then(str=>(new window.DOMParser).parseFromString(str,'text/xml')).then(data=>{for(var ls=data.querySelectorAll('url loc');;){var url=ls[Math.floor(Math.random()*ls.length)].innerHTML,url=new URL(url).pathname;if(location.pathname!==url)return void(location.pathname=url)}})}
